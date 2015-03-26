d7120e44db6d2dd276b97f347b5add92ff26daf9
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 05:03:44 +0000

    Some change
    git-svn-id: https://plugins.svn.wordpress.org/shaplatools/trunk@1121055 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/post-type/testimonials.php
+++ includes/post-type/testimonials.php
@@ -1,65 +1,79 @@
 <?php
 
-$labels = array(
-	'name'               => __( 'Testimonials', 'shapla' ),
-	'singular_name'      => __( 'Testimonial', 'shapla' ),
-	'add_new'            => __( 'Add New', 'shapla' ),
-	'add_new_item'       => __( 'Add New Testimonial', 'shapla' ),
-	'edit_item'          => __( 'Edit Testimonial', 'shapla' ),
-	'new_item'           => __( 'New Testimonial', 'shapla' ),
-	'view_item'          => __( 'View Testimonial', 'shapla' ),
-	'search_items'       => __( 'Search Testimonials', 'shapla' ),
-	'not_found'          => __( 'No Testimonials found', 'shapla' ),
-	'not_found_in_trash' => __( 'No Testimonials in trash', 'shapla' ),
-	'parent_item_colon'  => ''
-);
-
-$args = array(
-	'labels'              => $labels,
-	'public'              => false,
-	'exclude_from_search' => true,
-	'publicly_queryable'  => false,
-	'rewrite'             => array( 'slug' => 'testimonials' ),
-	'show_ui'             => true,
-	'query_var'           => true,
-	'capability_type'     => 'post',
-	'hierarchical'        => false,
-	'menu_position'       => 35,
-	'menu_icon'           => 'dashicons-format-chat',
-	'has_archive'         => false,
-	'supports'            => array( 'editor', 'thumbnail' )
-);
-
-register_post_type( 'testimonials', $args );
+	$labels = array(
+		'name'                => _x( 'Testimonials', 'Post Type General Name', 'shapla' ),
+		'singular_name'       => _x( 'Testimonial', 'Post Type Singular Name', 'shapla' ),
+		'menu_name'           => __( 'Testimonials', 'shapla' ),
+		'parent_item_colon'   => __( 'Parent Testimonial:', 'shapla' ),
+		'all_items'           => __( 'All Testimonials', 'shapla' ),
+		'view_item'           => __( 'View Testimonial', 'shapla' ),
+		'add_new_item'        => __( 'Add New Testimonial', 'shapla' ),
+		'add_new'             => __( 'Add New', 'shapla' ),
+		'edit_item'           => __( 'Edit Testimonial', 'shapla' ),
+		'update_item'         => __( 'Update Testimonial', 'shapla' ),
+		'search_items'        => __( 'Search Testimonial', 'shapla' ),
+		'not_found'           => __( 'Not found', 'shapla' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'shapla' ),
+	);
+	$args = array(
+		'label'               => __( 'testimonials', 'shapla' ),
+		'description'         => __( 'Post Type Description', 'shapla' ),
+		'labels'              => $labels,
+		'supports'            => array( 'editor', 'thumbnail', ),
+		'hierarchical'        => false,
+		'public'              => false,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 30,
+		'menu_icon'           => 'dashicons-format-chat',
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => false,
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => true,
+		'rewrite'             => array('slug' => 'testimonials',),
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'testimonial', $args );
 
 /**
  * Adding the necessary metabox
  *
  * This functions is attached to the 'testimonials_post_type()' meta box callback.
  */
-function testimonials_meta_boxes() {
-	add_meta_box( 'testimonials_form', 'Testimonial Details', 'testimonials_form', 'testimonials', 'normal', 'high' );
+function shapla_testimonials_meta_box() {
+	add_meta_box( 
+		'testimonials_form_id',
+		__('Testimonial Details','shapla'),
+		'shapla_testimonials_form', 
+		'testimonial', 
+		'normal', 
+		'high'
+	);
 }
-add_action( 'add_meta_boxes', 'testimonials_meta_boxes');
+add_action( 'add_meta_boxes', 'shapla_testimonials_meta_box');
 
 /**
  * Adding the necessary metabox
  *
  * This functions is attached to the 'add_meta_box()' callback.
  */
-function testimonials_form() {
-	$testimonial_data = get_post_meta( get_the_ID(), '_testimonial', true );
-	$client_name = ( empty( $testimonial_data['client_name'] ) ) ? '' : $testimonial_data['client_name'];
-	$source = ( empty( $testimonial_data['source'] ) ) ? '' : $testimonial_data['source'];
-	$link = ( empty( $testimonial_data['link'] ) ) ? '' : $testimonial_data['link'];
+function shapla_testimonials_form() {
+	// Add an nonce field so we can check for it later.
+	wp_nonce_field( 'shapla_testimonials_meta_box', 'shapla_testimonials_meta_box_nonce' );
 
-	wp_nonce_field( 'testimonials', 'testimonials' );
+	$testimonial = get_post_meta( get_the_ID(), '_testimonial', true );
+	$client_name = ( empty( $testimonial['client_name'] ) ) ? '' : $testimonial['client_name'];
+	$source = ( empty( $testimonial['source'] ) ) ? '' : $testimonial['source'];
+	$link = ( empty( $testimonial['link'] ) ) ? '' : $testimonial['link'];
+
 	?>
 	<table class="form-table">
 		<tr valign="top">
             <th scope="row">
                 <label for="client_name">
-                    <?php _e('Client\'s Name (optional)','shaplatools') ?>
+                    <?php _e('Client\'s Name (optional)','shapla') ?>
                 </label>
             </th>
 			<td>
@@ -69,7 +83,7 @@ function testimonials_form() {
 		<tr valign="top">
             <th scope="row">
                 <label for="source">
-                    <?php _e('Business/Site Name (optional)','shaplatools') ?>
+                    <?php _e('Business/Site Name (optional)','shapla') ?>
                 </label>
             </th>
 			<td>
@@ -79,7 +93,7 @@ function testimonials_form() {
 		<tr valign="top">
             <th scope="row">
                 <label for="link">
-                    <?php _e('Link (optional)','shaplatools') ?>
+                    <?php _e('Business/Site Link (optional)','shapla') ?>
                 </label>
             </th>
 			<td>
@@ -90,36 +104,54 @@ function testimonials_form() {
 	<?php
 }
 
-add_action( 'save_post', 'testimonials_save_post' );
+add_action( 'save_post', 'shapla_testimonials_save_post' );
 /**
  * Data validation and saving
  *
  * This functions is attached to the 'save_post' action hook.
  */
-function testimonials_save_post( $post_id ) {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+function shapla_testimonials_save_post( $post_id ) {
+	
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['shapla_testimonials_meta_box_nonce'] ) ) {
 		return;
-
-	if ( ! empty( $_POST['testimonials'] ) && ! wp_verify_nonce( $_POST['testimonials'], 'testimonials' ) )
-		return;
-
-	if ( ! empty( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
-		if ( ! current_user_can( 'edit_page', $post_id ) )
-			return;
-	} else {
-		if ( ! current_user_can( 'edit_post', $post_id ) )
-			return;
 	}
 
-	if ( ! wp_is_post_revision( $post_id ) && 'testimonials' == get_post_type( $post_id ) ) {
-		remove_action( 'save_post', 'testimonials_save_post' );
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['shapla_testimonials_meta_box_nonce'], 'shapla_testimonials_meta_box' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, it's safe for us to save the data now. */
+
+	if ( ! wp_is_post_revision( $post_id ) && 'testimonial' == get_post_type( $post_id ) ) {
+		remove_action( 'save_post', 'shapla_testimonials_save_post' );
 
 		wp_update_post( array(
 			'ID' => $post_id,
 			'post_title' => 'Testimonial - ' . $post_id
 		) );
 
-		add_action( 'save_post', 'testimonials_save_post' );
+		add_action( 'save_post', 'shapla_testimonials_save_post' );
 	}
 
 	if ( ! empty( $_POST['testimonial'] ) ) {
@@ -133,11 +165,11 @@ function testimonials_save_post( $post_id ) {
 	}
 }
 
-add_filter( 'manage_edit-testimonials_columns', 'testimonials_edit_columns' );
+add_filter( 'manage_edit-testimonial_columns', 'testimonials_edit_columns' );
 /**
  * Modifying the list view columns
  *
- * This functions is attached to the 'manage_edit-testimonials_columns' filter hook.
+ * This functions is attached to the 'manage_edit-testimonial_columns' filter hook.
  */
 function testimonials_edit_columns( $columns ) {
 	$columns = array(
@@ -147,7 +179,7 @@ function testimonials_edit_columns( $columns ) {
 		'testimonial-client-name' => 'Client\'s Name',
 		'testimonial-source' => 'Business/Site',
 		'testimonial-link' => 'Link',
-		'date' => 'Date'
+		'testimonial-avatar' => 'Client\'s Avatar'
 	);
 
 	return $columns;
@@ -176,6 +208,9 @@ function testimonials_columns( $column, $post_id ) {
 		case 'testimonial-link':
 			if ( ! empty( $testimonial_data['link'] ) )
 				echo $testimonial_data['link'];
+			break;
+		case 'testimonial-avatar':
+			echo get_the_post_thumbnail( get_the_ID(), array(64,64));
 			break;
 	}
 }
