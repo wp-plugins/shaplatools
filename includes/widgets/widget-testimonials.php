93309a7c81f427da2dd8cf8164c8a865e4a9e4d7
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 06:14:05 +0000

    Some change
    git-svn-id: https://plugins.svn.wordpress.org/shaplatools/trunk@1121083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/widgets/widget-testimonials.php
+++ includes/widgets/widget-testimonials.php
@@ -13,49 +13,14 @@ class Shapla_Testimonial extends WP_Widget {
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 		$posts_per_page = (int) $instance['posts_per_page'];
 		$orderby = strip_tags( $instance['orderby'] );
-		$testimonial_id = ( null == $instance['testimonial_id'] ) ? '' : strip_tags( $instance['testimonial_id'] );
 
 		echo $before_widget;
 
 		if ( ! empty( $title ) )
 			echo $before_title . $title . $after_title;
 
-		$args = array(
-			'posts_per_page' => (int) $posts_per_page,
-			'post_type' => 'testimonials',
-			'orderby' => $orderby,
-			'no_found_rows' => true,
-		);
-		if ( $testimonial_id )
-			$args['post__in'] = array( $testimonial_id );
-
-		$query = new WP_Query( $args  );
-
-		$testimonials = '';
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) : $query->the_post();
-			
-				$testimonial_data = get_post_meta( get_the_ID(), '_testimonial', true );
-				$client_name = ( empty( $testimonial_data['client_name'] ) ) ? '' : $testimonial_data['client_name'];
-				$client_source = ( empty( $testimonial_data['source'] ) ) ? '' : ' - ' . $testimonial_data['source'];
-				$client_link = ( empty( $testimonial_data['link'] ) ) ? '' : $testimonial_data['link'];
-				$cite = ( $client_link ) ? '<a href="' . esc_url( $client_link ) . '" target="_blank">' . $client_name . $client_source . '</a>' : $client_name . $client_source;
-				$testimonial_image= wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), array(64,64) );
-
-				$testimonials .= '<aside class="testimonial">';
-				$testimonials .= '<span class="quote">&ldquo;</span>';
-				$testimonials .= '<div class="entry-content">';
-				$testimonials .= '<p class="testimonial-text">' . get_the_content() . '<span></span></p>';
-				$testimonials .= '<span class="client-image"><img src="' . $testimonial_image[0] . '" width="' . $testimonial_image[1] . '" height="' . $testimonial_image[2] . '"></span>';
-				$testimonials .= '<span class="testimonial-client-name"><cite>' . $cite . '</cite></span>';
-				$testimonials .= '</div>';
-				$testimonials .= '</aside>';
-
-			endwhile;
-			wp_reset_postdata();
-		}
-
-		echo $testimonials;
+		if (function_exists('shapla_testimonials'))
+			echo shapla_testimonials($posts_per_page, $orderby);
 
 		echo $after_widget;
 	}
@@ -65,17 +30,15 @@ class Shapla_Testimonial extends WP_Widget {
 		$instance['title'] = strip_tags( $new_instance['title'] );
 		$instance['posts_per_page'] = (int) $new_instance['posts_per_page'];
 		$instance['orderby'] = strip_tags( $new_instance['orderby'] );
-		$instance['testimonial_id'] = ( null == $new_instance['testimonial_id'] ) ? '' : strip_tags( $new_instance['testimonial_id'] );
 
 		return $instance;
 	}
 
 	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'posts_per_page' => '1', 'orderby' => 'none', 'testimonial_id' => null ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'posts_per_page' => '1', 'orderby' => 'none' ) );
 		$title = strip_tags( $instance['title'] );
 		$posts_per_page = (int) $instance['posts_per_page'];
 		$orderby = strip_tags( $instance['orderby'] );
-		$testimonial_id = ( null == $instance['testimonial_id'] ) ? '' : strip_tags( $instance['testimonial_id'] );
 		?>
 		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
 		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
@@ -92,9 +55,6 @@ class Shapla_Testimonial extends WP_Widget {
 			<option value="modified" <?php selected( $orderby, 'modified' ); ?>>Modified</option>
 			<option value="rand" <?php selected( $orderby, 'rand' ); ?>>Random</option>
 		</select></p>
-
-		<p><label for="<?php echo $this->get_field_id( 'testimonial_id' ); ?>">Testimonial ID</label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'testimonial_id' ); ?>" name="<?php echo $this->get_field_name( 'testimonial_id' ); ?>" type="text" value="<?php echo $testimonial_id; ?>" /></p>
 		<?php
 	}
 }
