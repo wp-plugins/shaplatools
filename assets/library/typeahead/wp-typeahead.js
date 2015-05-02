fc01f28529c49c9dca4dad6334ba250f1815ab37
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-02 03:34:44 +0000

    Some changes
    git-svn-id: https://plugins.svn.wordpress.org/shaplatools/trunk@1151554 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/library/typeahead/wp-typeahead.js
@@ -0,0 +1,18 @@
+( function($) {
+	$( 'input[name="s"]' )
+		.typeahead( {
+			name: 'search',
+			remote: wp_typeahead.ajaxurl + '?action=ajax_search&fn=get_ajax_search&terms=%QUERY',
+			template: [
+				'<p><a href="{{url}}"><span class="s-img"><img src="{{img_url}}"></span><span class="s-title">{{value}}</span></a></p>',
+			].join(''),
+			engine: Hogan
+		} )
+		.keypress( function(e) {
+			if ( 13 == e.which ) {
+				$(this).parents( 'form' ).submit();
+				return false;
+			}
+		}
+	);
+} )(jQuery);
