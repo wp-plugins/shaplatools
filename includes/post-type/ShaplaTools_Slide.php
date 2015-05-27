2603ba6b418bf6bc160fe498a61016450b23127e
sayful <sayful@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 12:58:54 +0000

    Some changes in slide post type
    git-svn-id: https://plugins.svn.wordpress.org/shaplatools/trunk@1168483 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/post-type/ShaplaTools_Slide.php
+++ includes/post-type/ShaplaTools_Slide.php
@@ -4,9 +4,29 @@ if( !class_exists('ShaplaTools_Slide') ):
 
 class ShaplaTools_Slide {
 
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
 	public function __construct(){
 		add_action( 'init', array ($this, 'post_type') );
-		add_action( 'init', array ($this, 'taxonomy') );
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
 	}
 
 	/**
@@ -14,64 +34,52 @@ class ShaplaTools_Slide {
 	 * @package ShaplaTools
 	 * @link http://codex.wordpress.org/Function_Reference/register_post_type
 	 */
-	public static function post_type(){
+	public static function post_type() {
 
 		$labels = array(
-			'name'               => __( 'Slides', 'shapla' ),
-			'singular_name'      => __( 'Slide', 'shapla' ),
-			'add_new'            => __( 'Add New', 'shapla' ),
-			'add_new_item'       => __( 'Add New Slide', 'shapla' ),
-			'edit_item'          => __( 'Edit Slide', 'shapla' ),
-			'new_item'           => __( 'New Slide', 'shapla' ),
-			'view_item'          => __( 'View Slide', 'shapla' ),
-			'search_items'       => __( 'Search Slide', 'shapla' ),
-			'not_found'          => __( 'No Slides found', 'shapla' ),
-			'not_found_in_trash' => __( 'No Slides found in trash', 'shapla' ),
-			'parent_item_colon'  => ''
+			'name'                => _x( 'Slides', 'Post Type General Name', 'shapla' ),
+			'singular_name'       => _x( 'Slide', 'Post Type Singular Name', 'shapla' ),
+			'menu_name'           => __( 'Slide', 'shapla' ),
+			'name_admin_bar'      => __( 'Slide', 'shapla' ),
+			'parent_item_colon'   => __( 'Parent Slide:', 'shapla' ),
+			'all_items'           => __( 'All Slides', 'shapla' ),
+			'add_new_item'        => __( 'Add New Slide', 'shapla' ),
+			'add_new'             => __( 'Add New', 'shapla' ),
+			'new_item'            => __( 'New Slide', 'shapla' ),
+			'edit_item'           => __( 'Edit Slide', 'shapla' ),
+			'update_item'         => __( 'Update Slide', 'shapla' ),
+			'view_item'           => __( 'View Slide', 'shapla' ),
+			'search_items'        => __( 'Search Slide', 'shapla' ),
+			'not_found'           => __( 'Not found', 'shapla' ),
+			'not_found_in_trash'  => __( 'Not found in Trash', 'shapla' ),
 		);
-
 		$args = array(
+			'label'               => __( 'slide', 'shapla' ),
+			'description'         => __( 'Create slide for your site', 'shapla' ),
 			'labels'              => $labels,
+			'supports'            => array( 'title', 'thumbnail', ),
+			'hierarchical'        => false,
 			'public'              => false,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'menu_position'       => 25,
+			'menu_icon'           => 'dashicons-images-alt2',
+			'show_in_admin_bar'   => false,
+			'show_in_nav_menus'   => false,
+			'can_export'          => true,
+			'has_archive'         => false,
 			'exclude_from_search' => true,
 			'publicly_queryable'  => true,
-			'rewrite'             => array('slug' => 'slide'),
-			'show_ui'             => true,
-			'query_var'           => true,
+			'rewrite'             => false,
 			'capability_type'     => 'post',
-			'hierarchical'        => false,
-			'menu_position'       => 35,
-			'menu_icon'           => 'dashicons-images-alt2',
-			'has_archive'         => false,
-			'supports'            => array( 'title','editor', 'thumbnail' )
 		);
-
 		register_post_type( 'slide', $args );
-	}
 
-	/**
-	 * Register a skill taxonomy for slide post type.
-	 * @package ShaplaTools
-	 * @link https://codex.wordpress.org/Function_Reference/register_taxonomy
-	 */
-	public static function taxonomy(){
-		register_taxonomy( 'slide', 'slide', array(
-			'label'             => __( 'Slide Categories', 'shapla' ),
-			'singular_label'    => __( 'Slide Category', 'shapla' ),
-			'public'            => true,
-			'hierarchical'      => true,
-			'show_ui'           => true,
-			'show_in_nav_menus' => true,
-			'args'              => array( 'orderby' => 'term_order' ),
-			'query_var'         => true,
-			'rewrite'           => array( 'slug' => 'slide', 'hierarchical' => true)
-		) );
 	}
 }
 
 function run_shaplatools_slide(){
-	$shaplatools_slide = new ShaplaTools_Slide();
-	return $shaplatools_slide;
+	ShaplaTools_Slide::get_instance();
 }
 //run_shaplatools_slide();
 endif;
